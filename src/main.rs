1a4b7cc8fc246177edc82044ac9c0f590a191035
stdevHsequeda <henrysg88@gmail.com>
2020-02-05 14:59:37 -0500

    - Init. - Added guess.rs file. - Implementing guessing_game.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate rand;
 
-use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 use std::io;
+use rand::{Rng, thread_rng};
+use guess::Guess;
+
+mod guess;
 
 fn main() {
-    println!("Guess the number");
+    println!("Guess with   the number");
     let mut count = 0;
     println!("{}", count);
-    let secret_number = thread_rng().gen_range(1, 11);
-
+    let secret_number = thread_rng().gen_range(1, 100);
     loop {
         count = count + 1;
         println!("Please input your guess.");
@@ -19,15 +21,14 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
