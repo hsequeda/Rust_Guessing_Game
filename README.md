229f1822f25b0523c95249bdc2aae84c2cb7549a
stdevHsequeda <henrysg88@gmail.com>
2020-03-01 16:56:08 -0500

    Added the README.md file and the src/main.rs file.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rust_Guessing_Game
+A basic example project in Rust
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 extern crate rand;
 
+use guess::Guess;
+use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 use std::io;
-use rand::{Rng, thread_rng};
-use guess::Guess;
 
 mod guess;
 
 fn main() {
-    println!("Guess with   the number");
+    println!("Guess with the number");
     let mut count = 0;
-    println!("{}", count);
     let secret_number = thread_rng().gen_range(1, 100);
+
     loop {
         count = count + 1;
         println!("Please input your guess.");
